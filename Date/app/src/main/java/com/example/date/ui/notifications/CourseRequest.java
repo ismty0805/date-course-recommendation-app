4e7eb2b8d2d27db2f2eafaa9b27c6f0630649e9f
Taeyoung Kim <ismty0805@gmail.com>
2020-01-18 17:09:41 +0900

    test get course

---
+++ Date/app/src/main/java/com/example/date/ui/notifications/CourseRequest.java
@@ -0,0 +1,31 @@
+package com.example.date.ui.notifications;
+
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+import com.google.gson.JsonObject;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CourseRequest extends StringRequest {
+
+    final static private String URL = "http://192.249.19.252:2080/courses";
+    private Map<String, String> parameters;
+
+    public CourseRequest(String city, String level, String purpose, Response.Listener<String> listener) {
+        super(Method.PUT, URL, listener, null);
+        parameters = new HashMap<>();
+        parameters.put("city", city);
+        parameters.put("level", level);
+        parameters.put("purpose", purpose);
+    }
+
+    @Override
+    protected Map<String, String> getParams() throws AuthFailureError {
+        return parameters;
+    }
+}
--- Date/app/src/main/java/com/example/date/ui/notifications/NotificationsFragment.java
+++ Date/app/src/main/java/com/example/date/ui/notifications/NotificationsFragment.java
@@ -1,6 +1,7 @@
 package com.example.date.ui.notifications;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,9 +15,18 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
 import com.example.date.ui.account.LoginActivity;
 import com.example.date.R;
 import com.example.date.ui.account.SaveSharedPreference;
+import com.example.date.ui.mypage.PersonalInfoRequest;
+
+import org.json.JSONArray;
+
+import java.sql.Array;
+import java.util.ArrayList;
 
 public class NotificationsFragment extends Fragment {
 
@@ -34,6 +44,28 @@ public class NotificationsFragment extends Fragment {
                 startActivity(intent);
             }
         });
+
+        Response.Listener<String> responseListener = new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONArray jsonResponse = new JSONArray(response);
+                    ArrayList<String> latitudeArray = (ArrayList<String>) jsonResponse.getJSONObject(0).get("latitudeArray");
+                    ArrayList<String> longitudeArray = (ArrayList<String>) jsonResponse.getJSONObject(0).get("longitudeArray");
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+
+            }
+        };
+        CourseRequest courseRequest = new CourseRequest("대전", "3", "CONFLICT", responseListener);
+        RequestQueue queue = Volley.newRequestQueue(getActivity());
+        queue.add(courseRequest);
+
+
         return v;
     }
+
+
+
 }
